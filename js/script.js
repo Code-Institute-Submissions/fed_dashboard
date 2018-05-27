882b3629a3a69f47e9a8f4279b6316254a18c868
Van Mc G <00van12@gmail.com>
2018-05-27 13:11:29 +0000

    jquery file and script added. Button added to html file. Styles updated.

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    //adds slideTogggle to buttons to toggle criteria key section open and closed
+    $("#criteria_key").click(function(){
+        $("#key").slideToggle('1000');
+    });
+
+  //method chaining to add fade to button on mouserenter and mouseleave 
+    $("#criteria_key").mouseenter(function(){
+        $('#criteria_key').fadeTo(1000, 0.5);
+    });
+    $("#criteria_key").mouseleave(function(){
+        $('#criteria_key').fadeTo(1000, 1);
+    });
+});
+  
